deebb0b1e80b80477ad3d1e55d1b0a9c6fc8ebe0
kmacbeth29829 <mac16008@byui.edu>
2020-01-28 14:31:07 -0700

    update css

---
+++ lesson4/scripts/java-script.js
@@ -0,0 +1,56 @@
+const currentDate = new Date();
+console.log(currentDate);
+
+const currentYear = currentDate.getFullYear();
+console.log(currentYear);
+
+document.getElementById("currentYear").innerHTML = currentYear;  
+
+let dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = dayNames[d.getDay()];
+let monthName = monthNames[d.getMonth()];
+let fullDate = dayName + "," + monthName + "" + d.getDate() + " , " + d.getFullYear();
+document.getElementById("currentdate").innerHTML = fullDate;
+
+document.getElementById("lastModified").textContent = document.lastModified;
+
+// try{
+//     let options  = {
+//         weekday: "long",
+//         day: "numeric",
+//         month: "long",
+//         year: "numeric"
+//     };
+//     document.getElementById(
+//         "currentdate2"
+//     ).textContent = new Date().toLocaleDateString("en-US", options);
+
+// } catch (e) {
+//     alert("Error with code or you browser does not support Local");
+// }
+
