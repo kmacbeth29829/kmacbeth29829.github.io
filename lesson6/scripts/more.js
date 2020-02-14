071ede3800a60f6a7dd7b03799ed00933d5e1752
kmacbeth29829 <mac16008@byui.edu>
2020-02-14 14:55:46 -0700

    fix pictures

--- lesson6/scripts/more.js
+++ lesson6/scripts/more.js
@@ -1,7 +1,20 @@
 // weather summary
-document.querySelector('#temp').innerHTML = '58&deg;';
-document.querySelector('#wind_speed').innerHTML = '15 mph';
-document.querySelector('#humidity').innerHTML = '80&percnt;';
+
+let f, t, s;
+t = 50;
+s = 10;
+
+if (t <= 50 && s >= 3){
+f = 35.74 + 0.6215 * t - 35.75 *Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+} else{
+  f = 'N/A';
+  document.querySelector('#wind_chill').innerHTML = f;
+}
+
+document.querySelector('#temp').innerHTML = t + '&deg;';
+document.querySelector('#wind_speed').innerHTML = s + 'mph';
+document.querySelector('#wind_chill').innerHTML = f.toFixed(0) + '&deg;';
+
 
 // pancake1
 const currentDate = new Date();
