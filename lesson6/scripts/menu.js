2d1156754cf26e87752d5a8dc3a9bbcbba87808a
kmacbeth29829 <mac16008@byui.edu>
2020-02-15 21:40:57 -0700

    update css

--- lesson6/scripts/menu.js
+++ lesson6/scripts/menu.js
@@ -2,5 +2,6 @@ const hambutton = document.querySelector(".ham");
 hambutton.addEventListener("click", toggleMenu, false);
 
 function toggleMenu() {
+  // document.querySelector('#weather_summary').style.top = '700px';
   document.querySelector(".navigation").classList.toggle("responsive");
 }
