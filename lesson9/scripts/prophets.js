69e94900921e5f6c063ed16906fab9641a81e515
kmacbeth29829 <mac16008@byui.edu>
2020-03-05 18:19:16 -0700

    prophets

--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -8,13 +8,13 @@ fetch('https://byui-cit230.github.io/canvas-referenced/latter-day-prophets.json'
             prophet => {
                 let article = document.createElement('article')
 
-                let h1 = document.createElement('h1')
+                let h1 = document.createElement('h2')
                 h1.textContent = `${prophet.name} ${prophet.lastname}`;
 
-                let dateOfBirth = document.createElement('paragraph')
+                let dateOfBirth = document.createElement('h1')
                 dateOfBirth.textContent = ` Date of Birth: ${prophet.birthdate}`;
 
-                let placeOfBirth = document.createElement('paragraph')
+                let placeOfBirth = document.createElement('h1')
                 placeOfBirth.textContent = `place of birth: ${prophet.birthplace}`;
 
                 let image = document.createElement('img');
