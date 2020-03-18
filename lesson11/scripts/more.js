a44c49fdebdb1460da76cbdc937c2e17b143d4e5
kmacbeth29829 <mac16008@byui.edu>
2020-03-18 12:01:28 -0600

    new lesson

---
+++ lesson11/scripts/more.js
@@ -0,0 +1,15 @@
+// // weather summary
+// document.querySelector('#temp').innerHTML = '58&deg;';
+// document.querySelector('#wind_speed').innerHTML = '15 mph';
+// document.querySelector('#humidity').innerHTML = '80&percnt;';
+
+// // pancake1
+// const currentDate = new Date();
+// const aside = document.querySelector('aside')
+// if (currentDate.getDay() === 5) {
+//     aside.style.display = 'block';
+// } 
+// else{
+//     aside.style.display = 'none';
+// }
+
