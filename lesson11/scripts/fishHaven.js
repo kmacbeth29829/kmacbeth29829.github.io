1d9cddac737344363cf37592a327b608e9e055c3
kmacbeth29829 <mac16008@byui.edu>
2020-03-18 18:46:21 -0600

    new javascript

--- lesson11/scripts/fishHaven.js
+++ lesson11/scripts/fishHaven.js
@@ -61,32 +61,3 @@ fetch(apiURLForecast)
       }
     );
   });
-  // const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=141ca69a37402276d264debd535055f8&units=imperial"
-  // fetch(apiURL)
-  //     .then(response => response.json())
-  //     .then(
-  //         jsObject => {
-  //             console.log(jsObject)
-  //             let currentTemp = jsObject.main.temp
-  //             let windSpeed = jsObject.wind.speed
-  //             let windChill = 0
-  
-  //             if (currentTemp < 50 && windSpeed > 3) {
-  //                 windChill = 35.74 + 0.6215 * currentTemp - 35.75 *
-  //                     Math.pow(windSpeed, 0.16) + 0.4275 * currentTemp * Math.pow(windSpeed, 0.16);
-  //             }
-  //             document.getElementById('currently').textContent = jsObject.main.temp;
-  //             document.getElementById('high').textContent = jsObject.main.temp_max;
-  //             document.getElementById('wind_speed').textContent = jsObject.wind.speed;
-  //             document.getElementById('humidity').textContent = jsObject.main.humidity;
-  //             document.querySelector('#weather').textContent = jsObject.weather[0].main;
-  //             let imageURL = "https://openweathermap.org/img/w/" + jsObject.weather[0].icon + '.png';
-  //             document.querySelector("#weather_image").setAttribute('src', imageURL)
-  //             document.querySelector("#weather_image").setAttribute('alt', jsObject.weather[0].main);
-  //             if (windChill === 0) {
-  //                 document.getElementById("windChill").innerHTML = 'N/A';
-  //             } else {
-  //                 document.getElementById("windChill").innerHTML = Math.round(windChill) + '&deg;';
-  //             }
-  //         }
-  //     )
